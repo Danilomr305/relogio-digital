6924e82d609499fd2e888da36a3beab5a48fe2b4
Danilomr305 <danilomr305@gmail.com>
2023-06-17 11:32:22 -0300

    .

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,3 +3,13 @@ const minutos = document.getElementById('minutos');
 const segundos = document.getElementById('segundos');
 
 
+const relogio = setInterval(function time() {
+    let dateToday = new DataTransfer();
+    let hr = dateToday.getHours();
+    let min = dateToday.getMInutes();
+    let s = dateToday.getSeconds();
+
+    horas.textContent = hr;
+    minutos.textContent = min;
+    segundos.textContent= s;
+});
